dd9b5466c403fe0a03a22de5005b67378af12e6c
juliamcclellan <jmcclellan@mxschool.edu>
2016-05-13 19:46:44 -0400

    Controller class

---
+++ Controller.java
@@ -0,0 +1,17 @@
+/**
+ * The main class for the project.
+ * 
+ * @author Julia McClellan, Luke Giacalone, Hyun Choi
+ * @version 05/13/2016
+ */
+
+public class Controller
+{
+	/**
+	 * Runs the program.
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
+}
