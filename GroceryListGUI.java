abc21ac672dd2064139245499d0b005f153cda02
hyunchoi98 <hyunchoi98@gmail.com>
2016-05-20 11:15:01 -0400

    Emailing!!!

--- GroceryListGUI.java
+++ GroceryListGUI.java
@@ -14,7 +14,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Properties;
 
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -28,6 +30,12 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.naming.*;
+
+
 
 public class GroceryListGUI extends JFrame {
 	
@@ -82,10 +90,97 @@ public class GroceryListGUI extends JFrame {
 		});
 		panel.add(export, c);
 		
+		//adding email button
+		c.gridy++;
+		JButton email = new JButton("Email");
+		email.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String emailAddr = (String) JOptionPane.showInputDialog(null, "Enter your email address:", "Email My Shopping List", JOptionPane.PLAIN_MESSAGE);
+					if (!isValidEmail(emailAddr)) {
+						throw new IllegalArgumentException();
+					}
+					
+					String list = "Your shopping list generated at ";
+					list += new SimpleDateFormat("h:mm a 'on' MM/dd/yyyy").format(new java.util.Date());
+					list += ":\n";
+					for (Item i: groceries) {
+						list += i.getName() + " : " + i.amountToBuy() + "\n";
+					}
+					
+					if (!sendEmail(emailAddr, list)) { //attempt to send the email here
+						throw new RuntimeException();
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Email sent!", "Emailed list", JOptionPane.INFORMATION_MESSAGE);
+						return;
+					}
+					
+					
+				} 
+				catch (RuntimeException e1) { //two different error messages for different cases
+					JOptionPane.showMessageDialog(null, "Sending failed!", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				catch (Throwable e2) {
+					JOptionPane.showMessageDialog(null, "Invalid email address!", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+			}
+		});
+		panel.add(email, c);
+		
 		this.add(panel);
 		this.pack();
 		this.setResizable(false);
 		this.setVisible(true);
 	}
 	
+	//attempts to send an email, returns the success of such attempt
+	private boolean sendEmail(String emailAddr, String list) {
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+		
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication("hschoi9898","testForJava");
+				}
+		});
+
+			
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("hschoi9898@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(emailAddr));
+			message.setSubject("Your Shopping List");
+			message.setText(list);
+
+			Transport.send(message);
+		}
+		catch (Throwable e) {
+			return false;
+		}
+		
+		return true; 
+		
+
+	}
+	
+	//checks if a given string is a valid email address
+	private boolean isValidEmail(String s) {
+		try {
+			InternetAddress email = new InternetAddress(s);
+			email.validate();
+		}
+		catch (Throwable e) {
+			return false;
+		}
+		return true;
+	}
 }
